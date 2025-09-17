b6f9c9809046eeb8e77686aa574008d32e1a9a7a
JhonHeven06 <130428370+JhonHeven06@users.noreply.github.com>
2025-09-17 12:52:54 +0700

    Create Product.php
    Signed-off-by: JhonHeven06 <130428370+JhonHeven06@users.noreply.github.com>

---
+++ Task 14/Product.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+    
+    protected $fillable = [
+        'category_id',
+        'name',
+        'description',
+        'stock',
+        'image',
+    ];
+
+    /**
+     * Get the category that owns the product.
+     */
+    public function category()
+    {
+        return $this->belongsTo(ProductCategory::class, 'category_id');
+    }
+}
