d2118a89ef26821695e742e73e15ed294daaad02
JhonHeven06 <130428370+JhonHeven06@users.noreply.github.com>
2025-09-17 12:35:04 +0700

    Create homepage.blade.php
    Signed-off-by: JhonHeven06 <130428370+JhonHeven06@users.noreply.github.com>

---
+++ Task 12/homepage.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.main')
+
+@section('title', 'Halaman Utama')
+
+@section('content')
+    <h1>Selamat Datang di Toko Kami!</h1>
+    <p>Ini adalah halaman utama, di mana Anda bisa menemukan informasi dan promosi terbaru.</p>
+@endsection
