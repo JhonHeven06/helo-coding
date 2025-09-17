c19fa61ca583a3d78ab2b2385732131564ba16d8
JhonHeven06 <130428370+JhonHeven06@users.noreply.github.com>
2025-09-17 13:09:12 +0700

    Create navbar.blade.php
    Signed-off-by: JhonHeven06 <130428370+JhonHeven06@users.noreply.github.com>

---
+++ Task 16/components/navbar.blade.php
@@ -0,0 +1,7 @@
+<header>
+    <nav>
+        <a href="/">Halaman Utama</a>
+        <a href="{{ route('categories.index') }}">Daftar Kategori</a>
+        <a href="{{ route('products.index') }}">Daftar Produk</a>
+    </nav>
+</header>
