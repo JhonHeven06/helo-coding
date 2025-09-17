c4b98dab9fbbbfe5ed5c42799fa39a99b39db0b2
JhonHeven06 <130428370+JhonHeven06@users.noreply.github.com>
2025-09-17 17:11:31 +0700

    Delete Task 22/Controllers directory
    Signed-off-by: JhonHeven06 <130428370+JhonHeven06@users.noreply.github.com>

--- Task 22/Controllers/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\ProductCategoryController;
-
-Route::resource('products', ProductController::class);
-Route::resource('product-categories', ProductCategoryController::class);
