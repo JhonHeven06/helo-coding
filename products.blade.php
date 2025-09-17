49ad7e437894b0118a50e5941a6d13a1cef77fd5
JhonHeven06 <130428370+JhonHeven06@users.noreply.github.com>
2025-09-17 11:13:29 +0700

    Delete products.blade.php
    Signed-off-by: JhonHeven06 <130428370+JhonHeven06@users.noreply.github.com>

--- products.blade.php
+++
@@ -1,8 +0,0 @@
-<h1>Daftar Produk</h1>
-@foreach ($products as $product)
-    <div>
-        <h3>{{ $product->name }}</h3>
-        <p>{{ $product->description }}</p>
-        <p>Harga: ${{ $product->price }}</p>
-    </div>
-@endforeach
