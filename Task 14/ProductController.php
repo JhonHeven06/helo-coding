e2ed8e62d6993ca6301f9ebf804d7fa071a827bb
JhonHeven06 <130428370+JhonHeven06@users.noreply.github.com>
2025-09-17 12:53:33 +0700

    Create ProductController.php
    Signed-off-by: JhonHeven06 <130428370+JhonHeven06@users.noreply.github.com>

---
+++ Task 14/ProductController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+   
+    public function index()
+    {
+     
+        $products = Product::with('category')->paginate(10);
+
+        
+        return view('products', compact('products'));
+    }
+}
