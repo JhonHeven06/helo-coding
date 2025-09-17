029365d3092635544cb3b6277d1ec4f3d583fa47
JhonHeven06 <130428370+JhonHeven06@users.noreply.github.com>
2025-09-17 12:35:20 +0700

    Create products.blade.php
    Signed-off-by: JhonHeven06 <130428370+JhonHeven06@users.noreply.github.com>

---
+++ Task 12/products.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.main')
+
+@section('title', 'Daftar Produk')
+
+@section('content')
+    <h1>Daftar Produk</h1>
+    <ul>
+        <li>Produk A</li>
+        <li>Produk B</li>
+        <li>Produk C</li>
+    </ul>
+@endsection
